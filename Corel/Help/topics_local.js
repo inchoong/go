4ec8d781c52a4bb76b20f8f9b7e49feeb49d6957
LeGe <9572479+taoste@users.noreply.github.com>
2021-08-25 09:47:19 +0800

    【帮助说明文件】安装 CorelDRAW Graphics Suite 2018

---
+++ Corel/Help/topics_local.js
@@ -0,0 +1,23 @@
+function  WWHBookData_MatchTopic(P)
+  {
+  var C=null;
+  C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html";
+  if (P=="IDDH_DEFAULT") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_ADMINSN") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_ADMINDES") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_FILESUSE") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_FINERR") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_FINSUC") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_PREINSTALL") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_REBOOTPENDING") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_RESTMAN") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_LICENSE") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477487";
+  if (P=="IDDH_MINSYSD") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1477855";
+  if (P=="IDDH_SN") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1337396";
+  if (P=="IDDH_SNT") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1337396";
+  if (P=="IDDH_INSTALL_CHOOSE") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1479211";
+  if (P=="IDDH_INSTALL_PROGRAMS") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1489606";
+  if (P=="IDDH_INSTALL_FEATURES") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1480828";
+  if (P=="IDDH_ARP") C="CorelDRAW-Graphics-Suite-Frequently-Asked-Questions.html#1480747";
+  return C;
+  }
